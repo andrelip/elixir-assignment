344f74cea8af1250172fa493869f362dc090b0d8
André Stephano <andrestephano@gmail.com>
2020-04-17 06:50:36 -0300

    refactor: removes the WeatherForeast of the Assignment context to avoid confusion. When we move that to a proper library, we should add to prevent calling an external library directly at the network layer

--- lib/assignment_web/graphql/weather_resolver.ex
+++ lib/assignment_web/graphql/weather_resolver.ex
@@ -1,8 +1,6 @@
 defmodule AssignmentWeb.GraphQL.WeatherResolver do
   @moduledoc false
 
-  alias Assignment.WeatherForecast
-
   def weather_forecast(%{input: input}, _) do
     WeatherForecast.get(input.latitude, input.longitude)
   end
